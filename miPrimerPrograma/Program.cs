f978c500494e0df99fbe5ab2914393333a8a8d19
Julieta <ochoajulieta67@gmail.com>
2022-05-18 16:15:30 -0300

    Agregar archivos de proyecto.

---
+++ miPrimerPrograma/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var opcionElegida = "S";
+while (opcionElegida.ToUpper() == "S") 
+{
+    Console.Clear();
+    //Pedir el nombre de una persona
+
+    Console.WriteLine("Ingrese su nombre");
+    //Guardar el nombre en una variable
+
+    var nombrePersona = Console.ReadLine();
+    //Saludar 
+
+    Console.WriteLine("¡Hola " + nombrePersona + "!");
+
+    // Si desea o no continuar
+    Console.WriteLine("¿Desea continuar? S para si y N para no");
+    opcionElegida = Console.ReadLine();
+   
+}    
+
+if (opcionElegida.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente.");
+}
+else
+{
+    Console.WriteLine("Opcion no valida");
+}
+    
+     
+    
+
